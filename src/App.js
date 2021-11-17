9fee99f1e3109688678ffd914dffacf9ed8839cb
gabbyMill <gabbymillatiner@gmail.com>
2021-11-17 10:29:52 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <div className="container">
+        <h2>h2</h2>
+        <h3>h3</h3>
+        <h4>h4</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          reiciendis eius <strong>explicabo</strong> quaerat sit nesciunt,
+          officiis excepturi vero incidunt voluptatem?
+        </p>
+        <button>Click Me!</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
